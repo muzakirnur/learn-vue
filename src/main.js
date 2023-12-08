ec061fdb930006a900f7210a790fbc1c8391969d
Muzakir Nur <muzakir.nurr@gmail.com>
2023-12-08 08:56:08 +0700

    Installing Sweetalert and Make delete users

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import Swal from "sweetalert2";
 
+window.Swal = Swal;
 createApp(App).use(router).mount("#app");
--- src/views/Users/Index.vue
+++ src/views/Users/Index.vue
@@ -17,6 +17,16 @@ const fetchDataUsers = async () => {
 onMounted(() => {
   fetchDataUsers();
 });
+
+function deleteUsers(id) {
+  Api.post("/api/users/delete", { id: id }).then((response) => {
+    Swal.fire({
+      title: "Success!",
+      text: "User Deleted!",
+      icon: "success",
+    });
+  });
+}
 </script>
 
 <template>
@@ -26,13 +36,21 @@ onMounted(() => {
         <th class="p-2 text-left bg-slate-200">No</th>
         <th class="p-2 text-center bg-slate-200">Name</th>
         <th class="p-2 text-center bg-slate-200">Email</th>
-        <th class="p-2 text-right bg-slate-200">Email</th>
+        <th class="p-2 text-right bg-slate-200">Action</th>
       </thead>
       <tbody>
         <tr v-for="user in users">
           <td class="p-2 text-left">{{ no++ }}</td>
           <td class="p-2 text-center">{{ user.name }}</td>
           <td class="p-2 text-center">{{ user.email }}</td>
+          <td class="p-2 text-center">
+            <button
+              class="bg-red-500 hover:bg-red-600 text-white p-2 rounded-lg"
+              @click="deleteUsers(user.id)"
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
